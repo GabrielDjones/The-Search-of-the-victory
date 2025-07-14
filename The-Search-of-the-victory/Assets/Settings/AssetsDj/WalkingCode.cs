eee0cffa6b05f746dfa49949930c61b8d0aa9b52
GabrielDjones <gabriel.farias@alunos.sc.senac.br>
2025-07-14 13:35:39 -0300

    code de andar

---
+++ The-Search-of-the-victory/Assets/Settings/AssetsDj/WalkingCode.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class WalkingCode : MonoBehaviour
+{
+    private float eixoX;
+    private float eixoY;
+    public float speed;
+    private Rigidbody2D body;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        eixoX = Input.GetAxisRaw("Horizontal");
+        eixoY = Input.GetAxisRaw("Vertical");
+
+        body.linearVelocity = new Vector2(eixoX, eixoY) * speed;
+
+    }
+}
