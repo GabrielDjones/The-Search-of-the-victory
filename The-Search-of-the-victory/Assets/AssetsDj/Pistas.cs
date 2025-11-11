16be0a39fb287a3e1a5fdc62e2b42a850c2e475e
GabrielDjones <gabriel.farias@alunos.sc.senac.br>
2025-11-10 22:30:07 -0300

    correção de bugs

--- The-Search-of-the-victory/Assets/AssetsDj/Pistas.cs
+++ The-Search-of-the-victory/Assets/AssetsDj/Pistas.cs
@@ -25,7 +25,7 @@ public class Pistas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(hints >= 4)
+        if(hints >= 3)
         {
             entregarPistas.Invoke();
         }
